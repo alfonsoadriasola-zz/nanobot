ef6b21429d7c7aa3df1c747023da624e9181aad3
Alfonso Adriasola <aadriasola@cloudpassage.com>
2018-05-13 10:03:31 -0700

    some sins involved

--- nano.rb
+++ nano.rb
@@ -1,29 +1,52 @@
 require 'pry'
-
 class Brains
-  attr_accessor :cortex
-
-  def initialize(topology = { input: 738, p1: 32, p2: 16, output: 10 })
+  attr_accessor :cortex, :generation
+  def initialize(shape = { input: 738, hidden1: 16, hidden2: 16, output: 10 })
+    @generation = 0
     @cortex = {}
-    topology.each do |key, size|
-      arr = size.times.map { Neuron.new }
+    shape.each do |key, size|
+      arr = Array.new(size).map { Neuron.new }
       @cortex[key] = arr
     end
   end
+
+  def fire
+    @cortex[:hidden1].each(&:fire)
+  end
+
+  def input(array)
+    @generation += 1gs
+    @cortex[:input].each_with_index do |neuron, i|
+      neuron.activation = array[i]
+    end
+  end
+
+  def output(); end
 end
 
 class Neuron
-  attr_accessor :valence, :connections
+  attr_accessor :activation, :connections
   def initialize
-    valence = 0.50
-    connections = []
+    @activation = 0.00
+    @connections = []
+  end
+
+  def fire
+    sum = connections.reduce(activation) { |m, v| m + v }
+    @activation = 1.0 / (1.0 + Math.exp(- sum))
   end
 end
 
-def test
-  @brains = Brains.new
-
-  pp @brains
+# # # TESTS
+class BrainTest
+  def self.test
+    @brains = Brains.new
+    @brains.fire
+    sample= Array.new(738,1)
+    @brains.input(sample)
+    @brains.fire
+    pp @brains
+  end
 end
 
-test
+BrainTest.test
