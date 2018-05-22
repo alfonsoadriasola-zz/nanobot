3f79eeeae0ecde1e899b477827dc91b638e8ac75
Alfonso Adriasola <aadriasola@cloudpassage.com>
2018-05-22 15:26:11 -0700

    still thinking about it

--- nano.rb
+++ nano.rb
@@ -17,9 +17,10 @@ class Brains
     @cortex[:input].each_with_index do |neuron, i|
       neuron.activation = array[i]
     end
-    @cortex[:hidden1].each { |neuron| neuron.fire(@cortex) }
-    @cortex[:hidden2].each { |neuron| neuron.fire(@cortex) }
-    @cortex[:output].each { |neuron| neuron.fire(@cortex) }
+    @cortex[:input].each{ |neuron| neuron.fire(@cortex)}
+    @cortex[:hidden1].each { |neuron| neuron.fire(@cortex)}
+    @cortex[:hidden2].each { |neuron| neuron.fire(@cortex)}
+    @cortex[:output].each { |neuron| neuron.fire(@cortex)}
   end
 
   def output
@@ -51,7 +52,7 @@ class Neuron
   end
 
   def fire(cortex)
-    sum = connections(cortex).reduce(activation) { |m, v| m + v[0].to_f * v[1].to_f }
+    sum = connections(cortex).reduce(@activation) { |m, v| m + v[0].to_f * v[1].to_f }
     sum += bias
     @activation = 1.0 / (1.0 + Math.exp(- sum))
   end
@@ -59,53 +60,54 @@ class Neuron
   def backprop(cortex, correction)
     if connect_back_to
       cortex[connect_back_to].each_with_index do |n, i|
-        n.backprop(cortex, correction) && weights[i] += correction
+        weights[i] += correction
+        n.backprop(cortex, correction)
       end
+    else
+      fire(cortex) if activation > 0.9
     end
-    @activation += correction
   end
 end
 
 # # # TESTSi
 class BrainTest
   def self.test
-  
-    piczero = '
-      0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	1	1	1	1	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	0	0	0	0	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	0	0	0	0	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	0	0	0	0	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	0	0	0	0	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	0	0	0	0	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	0	0	0	0	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	0	0	0	0	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	0	0	0	0	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	0	0	0	0	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	0	0	0	0	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	1	1	1	1	1	1	1	1	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
-      0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
-    '.chomp.split('').map(&:to_f)
-    @brains = Brains.new(input: piczero.size)
-    @brains.input(piczero)
+    piczero =
+      '0000000000000000000000000000'\
+      '0000000000000000000000000000'\
+      '0000000000000000000000000000'\
+      '0000011111111111111111111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011110000000000001111000'\
+      '0000011111111111111111111000'\
+      '0000000000000000000000000000'\
+      '0000000000000000000000000000'\
+      '0000000000000000000000000000'
 
-    @brains.input(piczero)
-    expected = [1,0,0,0,0,0,0,0,0,0]
-    @brains.backprop expected
+    arr = piczero.split('').map(&:to_f)
+
+    @brains = Brains.new
     puts @brains.output
-    puts '--------------'
 
-    @brains.input(piczero)
-    @brains.backprop expected
+    @brains.input(arr)
+    puts @brains.output
 
+    @brains.backprop [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    puts @brains.output
+
+    @brains.input(arr)
     puts @brains.output
   end
 end
