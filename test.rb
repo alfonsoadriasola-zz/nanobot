09295dd0e018fb79609d4e78c66fa0036873d513
Alfonso Adriasola <aadriasola@cloudpassage.com>
2018-11-08 14:49:34 -0800

    change test style

--- test.rb
+++ test.rb
@@ -1,18 +1,24 @@
 require 'minitest/autorun'
 load 'nano2.rb'
 
-class TestNano < Minitest::Test
-  def test_this_shit
+describe NeuralNetwork do
+  def setup
     input_layer = Array.new(2)
     hidden_layer = Array.new(8)
     output_layer = Array.new(2)
 
-    nano = NeuralNetwork.new(
-            input_layer: input_layer,
-            hidden_layer: hidden_layer,
-            output_layer: output_layer
-            )
-    assert nano
-    assert nano.instance_variable_get('@weights1').size > 1 
+    @nano = NeuralNetwork.new(
+      input_layer: input_layer,
+      hidden_layer: hidden_layer,
+      output_layer: output_layer
+      )
+    end
+
+  it "works ok?" do
+    @nano.wont_be_nil
+  end
+
+  it "can have weights" do
+    assert @nano.instance_variable_get('@weights1').size > 1
   end
 end
